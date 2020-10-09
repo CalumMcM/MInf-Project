3e873dc4b7c32546450c6167b1b89876abb81152
Calum McM <calummcmeekin@me.com>
2020-10-09 10:59:51 +0100

    Added histogram manipulation

--- SatelliteSegmentation/ImagePrep/ImagePrep.py
+++ SatelliteSegmentation/ImagePrep/ImagePrep.py
@@ -6,6 +6,9 @@ from PIL import Image
 from os import listdir
 from os import remove
 import shutil
+from skimage import exposure
+from PIL import ImageEnhance
+from matplotlib import pyplot as plt
 
 class ImagePrep:
 
@@ -53,7 +56,10 @@ class ImagePrep:
     # Normalise the entire image
     def norm(self, band):
         band_min, band_max = band.min(), band.max()
+        #mean = np.mean(band)
+        #stand_dev = np.std(band)
         return ((band - band_min)/(band_max - band_min))
+        #return (band-mean)/stand_dev
 
     # Method to process the red band of the image (includes image stretch)
     def normalizeRed(self, intensity):
@@ -69,7 +75,8 @@ class ImagePrep:
         maxO    = 255
 
         iO      = (iI-minI)*(((maxO-minO)/(maxI-minI))+minO)
-
+        if (iO < 20):
+            return 0
         return iO
 
     # Method to process the green band of the image (includes image stretch)
@@ -86,7 +93,8 @@ class ImagePrep:
         maxO    = 255
 
         iO      = (iI-minI)*(((maxO-minO)/(maxI-minI))+minO)
-
+        if (iO < 20):
+            return 0
         return iO
 
     # Method to process the blue band of the image (includes image stretch)
@@ -104,15 +112,33 @@ class ImagePrep:
 
         iO      = (iI-minI)*(((maxO-minO)/(maxI-minI))+minO)
 
+        if (iO < 40):
+            return 0
+
         return iO
 
 
     # Delete the downloaded images, freeing up memory
     def deleteSource(self):
-        shutil.rmtree('Extracted/' + self.file_name)
-        remove('raw_images/' + self.file_name + '.tar.gz')
+        #shutil.rmtree('Extracted/' + self.file_name)
+        #remove('raw_images/' + self.file_name + '.tar.gz')
         pass
 
+    def linear_stretching(self, input, lower_stretch_from, upper_stretch_from):
+        """
+        Linear stretching of input pixels
+        :param input: integer, the input value of pixel that needs to be stretched
+        :param lower_stretch_from: lower value of stretch from range - input
+        :param upper_stretch_from: upper value of stretch from range - input
+        :return: integer, integer, the final stretched value
+        """
+
+        lower_stretch_to = 0  # lower value of the range to stretch to - output
+        upper_stretch_to = 255  # upper value of the range to stretch to - output
+
+        output = (input - lower_stretch_from) * ((upper_stretch_to - lower_stretch_to) / (upper_stretch_from - lower_stretch_from)) + lower_stretch_to
+
+        return output
     # Main method
     def prepareImage(self):
 
@@ -120,17 +146,23 @@ class ImagePrep:
 
         for i in range(len(bands[0])):
 
+
             b2_link = gdal.Open(bands[0][i])
             b3_link = gdal.Open(bands[1][i])
             b4_link = gdal.Open(bands[2][i])
 
+
+            print ("calculating norm")
             # call the norm function on each band as array converted to float
             b2 = self.norm(b2_link.ReadAsArray().astype(np.float))
             b3 = self.norm(b3_link.ReadAsArray().astype(np.float))
             b4 = self.norm(b4_link.ReadAsArray().astype(np.float))
 
+
             # Create RGB
             rgb = np.dstack((b4,b3,b2))
+            #rgb = np.dstack((b2_link.ReadAsArray().astype(np.float), b3_link.ReadAsArray().astype(np.float), b4_link.ReadAsArray().astype(np.float)))
+            #rgb = np.concatenate((b2, b3, b4))
             del b2, b3, b4
 
             # Visualize RGB
@@ -142,19 +174,95 @@ class ImagePrep:
             # Important: Here is where you can set the custom stretch
             # I use min as 2nd percentile and max as 98th percentile
             rgb = (rgb * 255).astype(np.uint8)
+
+            #-----Converting image to LAB Color model-----------------------------------
+            lab = cv2.cvtColor(rgb, cv2.COLOR_BGR2LAB)
+
+            #-----Splitting the LAB image to different channels-------------------------
+            l, a, b = cv2.split(lab)
+
+            #-----Applying CLAHE to L-channel-------------------------------------------
+            clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
+            cl = clahe.apply(l)
+
+            #-----Merge the CLAHE enhanced L-channel with the a and b channel-----------
+            limg = cv2.merge((cl,a,b))
+
+            #-----Converting image from LAB Color model to RGB model--------------------
+            final = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
+            cv2.imwrite(self.dir_path + 'final_before.png', final)
+
+            """
+            im = np.asarray(rgb)
+
+
+             # assign variable to max and min value of image pixels
+            max_value = np.max(im)
+            min_value = np.min(im)
+            print (max_value)
+            print (min_value)
+
+            print (im.shape())
+            im = (im - min_value) * ((255-0)/(max_value-min_value))
+            print (im.shape())
+            #cv2.imwrite(self.dir_path + 'mergedNotContrasted.tif', im)
+            """
             im = Image.fromarray(rgb)
+
             im.save(self.dir_path + 'mergedNotContrasted.tif')
 
-            # Create an image object
+            #-----Reading the image-----------------------------------------------------
+            img = cv2.imread(self.dir_path + 'mergedNotContrasted.tif', 1)
 
+            #-----Converting image to LAB Color model-----------------------------------
+            lab= cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+
+            #-----Splitting the LAB image to different channels-------------------------
+            l, a, b = cv2.split(lab)
+
+            #-----Applying CLAHE to L-channel-------------------------------------------
+            clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
+            cl = clahe.apply(l)
+
+            #-----Merge the CLAHE enhanced L-channel with the a and b channel-----------
+            limg = cv2.merge((cl,a,b))
+
+            #-----Converting image from LAB Color model to RGB model--------------------
+            final = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
+            cv2.imwrite(self.dir_path + 'final_after.png', final)
+
+            # Create an image object
             imageObject     = Image.open(self.dir_path + "mergedNotContrasted.tif")
 
-            # Split the red, green and blue bands from the Image
+            # Get histogram
 
+            img = cv2.imread(self.dir_path + "final_after.png")
+            color = ('b','g','r')
+            for i,col in enumerate(color):
+                histr = cv2.calcHist([img],[i],None,[256],[0,256])
+                plt.plot(histr,color = col)
+                plt.xlim([0,256])
+            plt.show()
+
+            """
+            print (imageObject)
+            hist, bins = np.histogram(imageObject, bins=50)
+
+            width = 0.7 * (bins[1] - bins[0])
+            center = (bins[:-1] + bins[1:]) / 2
+            plt.bar(center, hist, align='center', width=width)
+            plt.title("Gaussian Histogram")
+            plt.xlabel("Value")
+            plt.ylabel("Frequency")
+            plt.show()
+            """
+            #enhancer_object = ImageEnhance.Contrast(imageObject)
+            #normalizedImage = enhancer_object.enhance(3)
+
+            # Split the red, green and blue bands from the Image
             multiBands      = imageObject.split()
 
             # Apply point operations that does contrast stretching on each color band
-
             normalizedRedBand      = multiBands[0].point(self.normalizeRed)
 
             normalizedGreenBand    = multiBands[1].point(self.normalizeGreen)
@@ -162,18 +270,31 @@ class ImagePrep:
             normalizedBlueBand     = multiBands[2].point(self.normalizeBlue)
 
 
-
             # Create a new image from the contrast stretched red, green and blue brands
-
             normalizedImage = Image.merge("RGB", (normalizedRedBand, normalizedGreenBand, normalizedBlueBand))
 
             # Rotate the image
             normalizedImage = normalizedImage.rotate(12.4)
 
             # Crop the image
-            normalizedImage = normalizedImage.crop((691,607,6908,7140))
+            normalizedImage = normalizedImage.crop((691,630,6890,7100))
+
 
             # Save the image
             normalizedImage.save('data/images/' + self.file_name + '.tif')
 
+            #img = cv2.imread('data/images/' + self.file_name + '.tif' ,0)
+            #plt.hist(b2,256,[0,256]); plt.show()
+
         self.deleteSource()
+
+
+if __name__ == "__main__":
+    #imagePrep = ImagePrep("LC080900862013101101T1-SC20200930162215")
+    #imagePrep.prepareImage();
+
+    #imagePrep = ImagePrep("LC080900862019072401T1-SC20200930162234")
+    #imagePrep.prepareImage();
+
+    imagePrep = ImagePrep("LC080900862020050701T1-SC20200930161922")
+    imagePrep.prepareImage();
