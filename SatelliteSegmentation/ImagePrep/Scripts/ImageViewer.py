f353fdbd78785034486529985d13a84cf8cd2fa4
Calum McM <calummcmeekin@me.com>
2020-11-07 11:12:28 +0000

    Revert "Added Cerrado"
    This reverts commit 6ae7a4a9bb062c23da68b25f1fb4c5e33e36773f.

--- SatelliteSegmentation/ImagePrep/Scripts/ImageViewer.py
+++ SatelliteSegmentation/ImagePrep/Scripts/ImageViewer.py
@@ -6,7 +6,6 @@ from rasterio.plot import show
 import xarray as xr
 import georaster
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 from osgeo import gdal
 from os import listdir
 from os.path import isfile, join
@@ -31,26 +30,6 @@ gdal.Translate(save_dir,
                options=options_string)
 """
 
-os_dir = '/Users/calummcmeekin/Documents/GitHub/MInf-Project/SatelliteSegmentation/ImagePrep/Labelled/'
-
-def plot_group(biome):
-    path = os_dir + biome + '/png'
-
-    all_files = [f for f in listdir(path) if isfile(join(path, f)) ]
-    png_files = [f for f in all_files if not 'png.aux.xml' in f and not '.DS_Store' in f]
-    print (len(png_files))
-    w=10
-    h=10
-    fig=plt.figure(figsize=(8, 8))
-    columns = 5
-    rows = 6
-    for i in range(1, columns*rows +1):
-        img = mpimg.imread(path + '/' + png_files[i-1])
-        fig.add_subplot(rows, columns, i)
-        plt.axis('off')
-        plt.imshow(img)
-    plt.savefig(os_dir + biome + '/all_classes.png')
-    plt.show()
 
 # Takes in the band of an image and saves the matrix
 # for that band to a .txt file
@@ -75,45 +54,39 @@ def view_image(image_dir):
     show(img.read([1,2,3]))
 
 def view_batch_image(image_dir):
-    files = [f for f in listdir(image_dir) if isfile(join(image_dir, f)) and '.DS_Store' not in f]
+    files = [f for f in listdir(image_dir) if isfile(join(image_dir, f))]
 
     iter = 1
 
     for file in files:
-        fp = r'/Users/calummcmeekin/Documents/GitHub/MInf-Project/SatelliteSegmentation/ImagePrep/Labelled/Cerrado/tif/' + file
+        fp = r'/Users/calummcmeekin/Documents/GitHub/MInf-Project/SatelliteSegmentation/ImagePrep/Labelled/Caatinga/tif/' + file
         img = rs.open(fp)
 
         # Print number of bands in the image:
         show(img.read([1,2,3]))
 
-def convertPNG(biome):
+def convertJPG(image_dir):
 
-    image_dir = os_dir + biome + '/tif'
+   files = [f for f in listdir(image_dir) if isfile(join(image_dir, f))]
 
-    files = [f for f in listdir(image_dir) if isfile(join(image_dir, f)) and '.DS_Store' not in f]
+   iter = 1
 
+   for file in files:
+       iter += 1
+       print (file)
+       save_dir = r'/Users/calummcmeekin/Documents/GitHub/MInf-Project/SatelliteSegmentation/ImagePrep/Labelled/Caatinga/png/' + file.split('.')[0] + '.png'
+       file_dir = r'/Users/calummcmeekin/Documents/GitHub/MInf-Project/SatelliteSegmentation/ImagePrep/Labelled/Caatinga/tif/' + file
+       scale = '-scale min_val max_val'
 
-    iter = 1
+       print (file_dir)
+       options_list = [
+           '-ot Byte',
+           '-of PNG',
+           scale
+       ]
+       options_string = " ".join(options_list)
 
-    for file in files:
-        iter += 1
-
-        print (file)
-
-        save_dir = os_dir + biome + '/png/' + file.split('.')[0] + '.png'
-        file_dir = os_dir + biome + '/tif/' + file
-
-        scale = '-scale min_val max_val'
-
-        print (file_dir)
-        options_list = [
-            '-ot Byte',
-            '-of PNG',
-            scale
-        ]
-        options_string = " ".join(options_list)
-
-        gdal.Translate(save_dir, file_dir, options=options_string)
+       gdal.Translate(save_dir, file_dir, options=options_string)
 
 
 
@@ -121,10 +94,7 @@ if __name__ == "__main__":
 
     MapBiome_data = '/Users/calummcmeekin/Downloads/mosaic-2019-0000000000-0000000000.tif'
     Landsat_image = '/Users/calummcmeekin/Downloads/LE07_220076_20000608.tif'
+    #convertJPG(r'/Users/calummcmeekin/Documents/GitHub/MInf-Project/SatelliteSegmentation/ImagePrep/Labelled/Caatinga/tif')
 
-    convertPNG('Amazonia')
-
-    #view_batch_image(r'/Users/calummcmeekin/Documents/GitHub/MInf-Project/SatelliteSegmentation/ImagePrep/Labelled/Cerrado/tif')
+    view_batch_image(r'/Users/calummcmeekin/Documents/GitHub/MInf-Project/SatelliteSegmentation/ImagePrep/Labelled/Caatinga/tif')
     #view_image(Landsat_image)
-
-    plot_group("Amazonia")
