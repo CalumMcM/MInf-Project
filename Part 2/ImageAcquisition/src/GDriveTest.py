b93afe91edad1a5058546d3ddeeba0da88b8c72e
CalumMcM <calummcmeekin@me.com>
2021-10-22 11:07:11 +0100

    GDriveTest

--- Part 2/ImageAcquisition/src/GDriveTest.py
+++ Part 2/ImageAcquisition/src/GDriveTest.py
@@ -6,12 +6,26 @@ def main():
     drive = GoogleDrive(gauth)
 
     DIR = r'/Volumes/GoogleDrive/My\ Drive/PlanetScope\ Imagery/Quads/'
-    upload_file = 'test.txt'
+    upload_file = '632-1034.tiff'
+    """
 
-    gfile = drive.CreateFile({'parents': [{'id': '1vma9es9Kd36m8pIrTSEkZ5J3C-nY66ao'}]})
-    # Read file and set it as the content of this instance.
-    gfile.SetContentFile(upload_file)
-    gfile.Upload() # Upload the file.
+    """
+    get_files(gauth, drive)
+    #gfile = drive.CreateFile({'parents': [{'id': '1vma9es9Kd36m8pIrTSEkZ5J3C-nY66ao'}]})
+    #gfile.SetContentFile(upload_file)
+    #gfile.Upload() # Upload the file.
+
+def get_files(gauth, drive):
+    # Check if file exists
+    biome_dir_query = "'1MA0AUgR_ucfXc5OGvDadF2AzVDUXG_9U' in parents and trashed=false"
+    file_list = drive.ListFile({'q': "'1uq0IW28Sb7r3rtzW5_V_3cZJ7W7K16za' in parents and trashed=false"}).GetList()
+    file_names = [file['title'] for file in file_list]
+
+    print (file_names)
+    #for file1 in file_list:
+    #    print (file1['title'])
+        #if file1['title'] == gpath:
+            #id = file1['id']
 
 if __name__ == "__main__":
     main()
